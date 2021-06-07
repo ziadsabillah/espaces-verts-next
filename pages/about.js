365bfdb4dedcd8ec334f55f41f7b61afce1e4242
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-06-07 20:46:13 +0100

    added about page skeleton

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+export default function AboutPage() {
+    return (
+        <>
+        
+        <Layout title="Qui sommes nous?">
+
+        </Layout>
+
+        </>
+    )
+}
