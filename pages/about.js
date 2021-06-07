2db1faddddd5c50bb80360b69657c85fbe15a736
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-06-07 21:04:10 +0100

    added first part of about page

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,80 @@
+import { Container, Row, Col, Image } from "react-bootstrap";
 import Layout from "../components/Layout";
+import { SectionHeader } from "../components/SectionHeader";
+import styled from 'styled-components'
+
+const List = styled.ul`
+    position: relative;
+
+    li {
+        position: relative;
+        color: #242424;
+        font-size: 20px;
+        padding-left: 40px;
+        margin-bottom: 15px;
+    }
+
+    li::before {
+        position: absolute;
+        content: ' ';
+        background: url('/icons/check.png') center top;
+        background-repeat: no-repeat;
+        background-size: contain;
+        left: 0px;
+        top: 0px;
+        height: 20px;
+        width: 20px;
+        line-height: 1em;
+        font-size: 18px;
+
+    }
+`;
 
 export default function AboutPage() {
     return (
         <>
         
         <Layout title="Qui sommes nous?">
-
+            <Container>
+                <SectionHeader>
+                    <h2>Qui sommes nous?</h2>
+                </SectionHeader>
+                <Row>
+                    <Col sm={12} md={6}>
+                        <h2>Nous sommes Jardinier SARL.</h2>
+                        <p>
+                        Nous sommes une équipe d'experts professionnels 
+                        et qualifiés dans toutes les sphères domestiques.
+                         Nous offrons une large gamme de services et en même
+                          temps, nous sommes toujours heureux de vous aider
+                           pour tous les besoins domestiques non conventionnels :
+                        </p>
+                        <List>
+                            <li>
+                            Élagage d'arbres et d'arbustes
+                            </li>
+                            <li>
+                            Entretien du lit de jardin
+                            </li>
+                            <li>
+                            Irrigation & Drainage
+                            </li>
+                            <li>
+                            Entretien de la pelouse et du jardin
+                            </li>
+                            <li>
+                            Planting & Removal
+                            </li>
+                            <li>
+                            Déneigement et déglaçage
+                            </li>
+                        </List>
+                    </Col>
+                    <Col sm={12} md={6}>
+                        <Image src="/img/about.png" alt="Qui sommes Nous Image" />
+                    </Col>
+                </Row>
+            </Container>
         </Layout>
 
         </>
