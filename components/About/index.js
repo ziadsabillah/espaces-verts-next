a03f4e6b5df21761718277f809b055de67be5787
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-06-07 21:57:51 +0100

    added why choose us component

---
+++ components/About/index.js
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+import { Container, Row, Col, Image } from "react-bootstrap";
+import { SectionHeader } from "../SectionHeader";
+import WhyChooseUs from './whychooseus';
+
+
+const AboutSection = styled.section`
+
+    padding-bottom: 150px;
+    padding-top: 40px;
+
+`;
+
+
+const List = styled.ul`
+    position: relative;
+
+    li {
+        position: relative;
+        color: #242424;
+        font-size: 20px;
+        padding-left: 40px;
+        margin-bottom: 15px;
+    }
+
+    li::before {
+        position: absolute;
+        content: ' ';
+        background: url('/icons/check.png') center top;
+        background-repeat: no-repeat;
+        background-size: contain;
+        left: 0px;
+        top: 0px;
+        height: 20px;
+        width: 20px;
+        line-height: 1em;
+        font-size: 18px;
+
+    }
+`;
+
+const AboutImage = styled(Image)`
+        max-width: 100%;
+    
+`;
+
+export default function About() {
+    return (
+        <Container>
+            <AboutSection>
+
+                <SectionHeader>
+                    <h2>Qui sommes nous?</h2>
+                </SectionHeader>
+                <Row>
+                    <Col sm={12} md={6}>
+                        <h2>Nous sommes Jardinier SARL.</h2>
+                        <p>
+                            Nous sommes une équipe d'experts professionnels
+                            et qualifiés dans toutes les sphères domestiques.
+                            Nous offrons une large gamme de services et en même
+                            temps, nous sommes toujours heureux de vous aider
+                            pour tous les besoins domestiques non conventionnels :
+                        </p>
+                        <List>
+                            <li>
+                                Élagage d'arbres et d'arbustes
+                            </li>
+                            <li>
+                                Entretien du lit de jardin
+                            </li>
+                            <li>
+                                Irrigation & Drainage
+                            </li>
+                            <li>
+                                Entretien de la pelouse et du jardin
+                            </li>
+                            <li>
+                                Planting & Removal
+                            </li>
+                            <li>
+                                Déneigement et déglaçage
+                            </li>
+                        </List>
+                    </Col>
+                    <Col sm={12} md={6}>
+                        <AboutImage src="/img/about.png" alt="Qui sommes Nous Image" />
+                    </Col>
+                </Row>
+            </AboutSection>
+            <WhyChooseUs />
+        </Container>
+    )
+}
---
+++ components/About/whychooseus.js
@@ -0,0 +1,96 @@
+import { Row, Col } from 'react-bootstrap';
+import styled from 'styled-components'
+import { FcOvertime } from 'react-icons/fc'
+import { IoIosCash, IoMdMedal, IoMdTime, } from 'react-icons/io'
+import {AiOutlineStar} from 'react-icons/ai';
+import { SectionHeader } from '../SectionHeader';
+const Section = styled.section`
+
+    position: relative;
+    padding: 50px 0;
+`;
+
+const CardWrapper = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+const Icon = styled.div`
+    margin: 1.4em 0;
+
+`;
+
+const Title = styled.h4`
+    font-size: 20px;
+    color: #242424;
+
+`;
+const Paragraph = styled.p`
+    font-style: 14px;
+    color: #79787f;
+`;
+
+const data = [
+    {
+        id: 0,
+        title: 'Toujours Disponible',
+        icon: <IoMdTime color="green" size={80} />,
+        description: `We accept requests and phone calls 24/7 so you could resolve any problem whenever you need. Our emergency team will be at your place to fix the breakdown at short notice.`
+    },
+    {
+        id: 1,
+        title: 'Agents qualifiés',
+        icon: <IoMdMedal color="green" size={80} />,
+        description: `
+        Tous les membres de notre équipe sont des agents hautement qualifiés, instruits et compétents. Tous sont formés selon les dernières technologies. Nos débutants travaillent avec des collègues expérimentés pour étudier tous les détails.
+        `
+    },
+    {
+        id: 2,
+        title: 'Des prix équitables',
+        icon: <IoIosCash color="green" size={80} />,
+        description: `
+        Nos prix sont à la fois justes et abordables pour tous. Nous offrons un système de remise flexible afin que vous puissiez utiliser n'importe quel service dont vous avez besoin.
+        `
+    },
+    {
+        id: 3,
+        title: 'Meilleures offres',
+        icon: <AiOutlineStar color="green" size={80} />,
+        description: `
+        Nous offrons des promos sur les services les plus populaires et sur les services saisonniers, vous pouvez donc certainement recevoir de l'aide sans délai.
+        `
+    }
+]
+
+const WhyChooseUs = () => {
+    return (
+        <>
+            <Section>
+                <SectionHeader>
+                    <h2>Pourquoi nous?</h2>
+                </SectionHeader>
+                <Row>
+                    {data?.map(item => (
+                        <Col key={item.id} sm={12} md={3}>
+                            <CardWrapper>
+                                <Icon>
+                                    {item.icon}
+                                </Icon>
+                                <Title>{item.title}</Title>
+                                <Paragraph>
+                                    {item.description}
+                                </Paragraph>
+                            </CardWrapper>
+                        </Col>
+                    ))}
+                </Row>
+            </Section>
+
+        </>
+    )
+}
+
+export default WhyChooseUs;
