3c17016f434a883e8a12959df60d0ac9b3259144
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-06-08 19:02:22 +0100

    added projects page skeleton

---
+++ pages/projects.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import Layout from '../components/Layout';
+
+
+const Projects = () => {
+    return (
+        <>
+        <Layout title="Nos Projets">
+            
+        </Layout>
+        </>
+    )
+}
+
+export default Projects;
