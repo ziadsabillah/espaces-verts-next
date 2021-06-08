c1274e63f5af8a50b9b8c3a69109336d39152f4d
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-06-08 19:14:35 +0100

    added section header

--- pages/projects.js
+++ pages/projects.js
@@ -1,12 +1,18 @@
+import { Container } from 'react-bootstrap';
 import styled from 'styled-components'
 import Layout from '../components/Layout';
+import {SectionHeader} from '../components/SectionHeader'
 
 
 const Projects = () => {
     return (
         <>
         <Layout title="Nos Projets">
-            
+            <Container>
+                <SectionHeader>
+                    <h2>Réalisations</h2>
+                </SectionHeader>
+            </Container>
         </Layout>
         </>
     )
